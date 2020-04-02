517eafef95d2b08661812e26eff367982963b7e2
超人不会飞 <594008635@qq.com>
2020-04-02 11:15:22 +0800

    初始化项目，整理文件目录

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>home1111111111111111111111</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+
+<style>
+
+</style>
